732aebd11c6ad3a89d87f30cb7b4a3f532f5f2d4
sanyuan <sanyuan.jw@foxmail.com>
2020-11-28 17:15:37 +0800

    feat: 添加首页功能模块组件

---
+++ dy-mini/pages/home/modules/modules.js
@@ -0,0 +1,29 @@
+// pages/home/modules/modules.js
+Component({
+  data: {
+    modules: [
+      {
+        id: 1,
+        name: '融脸'
+      },
+      {
+        id: 2,
+        name: '动漫化'
+      },
+      {
+        id: 3,
+        name: '美妆'
+      },
+      {
+        id: 4,
+        name: '美图秀秀'
+      },
+    ]
+  },
+  properties: {
+
+  },
+  methods: {
+
+  }
+})
